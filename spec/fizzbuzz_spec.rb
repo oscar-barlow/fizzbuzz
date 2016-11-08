3cdd506df5bb148cf237f43b7f463badbae95f25
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-08 10:22:21 +0000

    complete second solution

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,16 @@ describe "Fizzbuzz" do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 
+  it 'returns "fizz when given a number divisible by only 3"' do
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+
+  it 'returns "buzz when given a number divisibly by only 5"' do
+    expect(fizzbuzz(25)).to eq "buzz"
+  end
+
+  it 'returns the number it\'s given if that number isn\'t divisible by three or 5' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+
 end
