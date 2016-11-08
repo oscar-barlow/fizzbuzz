f845acfb2b11f63e54acd766152cf6ae44546d0c
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-08 09:58:31 +0000

    wipe it clean

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,1 @@
-def fizzbuzz(n)
-  return "fizzbuzz" if n % 3 == 0 && n % 5 == 0
-  return "fizz" if n % 3 == 0
-  return "buzz" if n % 5 == 0
-end
+WIPE IT CLEAN
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,1 @@
-require 'fizzbuzz'
-
-describe 'Fizzbuzz' do
-  it 'returns "fizz" when given a number divisible by 3' do
-    expect(fizzbuzz(6)).to eq 'fizz'
-  end
-
-  it 'returns "buzz" when given a number divisible by 5' do
-    expect(fizzbuzz(10)).to eq 'buzz'
-  end
-
-  it 'returns "fizzbuzz" when given a number divisible by both 3 and 5' do
-    expect(fizzbuzz(30)).to eq 'fizzbuzz'
-  end
-
-end
+WIPE IT CLEAN
