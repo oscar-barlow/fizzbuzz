3492b75f4a2aa58be2d2e2b9089e45ca851aaad7
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-09 10:10:44 +0000

    check divisbility and non-divisibility by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,12 +8,20 @@ describe "fizzbuzz" do
       expect(is_divisible_by_3?(3)).to eq true
     end
 
+    it 'checks for divisibility by 5' do
+      expect(is_divisible_by_5?(5)).to eq true
+    end
+
   end
 
   context "when given a number that fizzbuzz should just return," do
 
     it 'checks for indivisibility by 3' do
       expect(is_divisible_by_3?(4)).to eq false
+    end
+
+    it 'checks for indivisibility by 5' do
+      expect(is_divisible_by_5?(6)).to eq false
     end
 
   end
